27399a9f25cd538691791a777083580c24cc18d3
kathyxie88 <katherinexy@hotmail.com>
2026-06-04 17:09:33 +0800

    Add reset button

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,14 @@ function App() {
       <h1>Hello Kathy</h1>
       <p>这是我的第一个 React 小功能。</p>
 
+      <p>当前数字：{count}</p>
+
       <button onClick={() => setCount(count + 1)}>
-        点击次数：{count}
+        +1
+      </button>
+
+      <button onClick={() => setCount(0)}>
+        重置
       </button>
     </main>
   )
